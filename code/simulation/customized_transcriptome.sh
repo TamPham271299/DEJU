4503939b50538e39a9d828c25ca38153b716f64a
TamPham <phammaitam99@gmail.com>
2025-06-17 16:38:38 +0700

    Update customized_transcriptome.sh

--- code/simulation/customized_transcriptome.sh
+++ code/simulation/customized_transcriptome.sh
@@ -79,3 +79,46 @@ sort -k4,4 -o AS_sorted.bed12.tsv AS.bed12.tsv
 ### Extract sequences from reference FASTA based on genomic intervals in BED12
 bedtools getfasta -fi $FASTA -bed AS_sorted.bed12.tsv -split -nameOnly -fo gencode.vM32.custom.transcriptome.tmp.fa
 fold -w 60 gencode.vM32.custom.transcriptome.tmp.fa > gencode.vM32.custom.transcriptome.fa
+
+##############################################################################################################################
+# # Function: From genomic intervals to transcriptome
+# # Input: Genomic intervals of transcripts designed for alternative splicing pattern (BED12)
+# # Output: Customized transcriptome (FASTA) with 5 transcripts per gen
+
+# module load bedtools/2.31.1
+
+# cd ../../data/simulation/customized_transcriptome
+
+# FASTA="../../../annotation/GRCm39.primary_assembly.genome.fa"
+
+# ### ES
+# bedtools groupby -i <(sed '1d' ES_5tr.pre.bed12.tsv) -g 6 -c 1,2,3,6,4,6,8,9 -o first,min,max,first,first,count,collapse,collapse| \
+#   awk -F'\t' 'BEGIN{OFS="\t";}{print $2, $3, $4, $5, "0", $6, $4, $4, "0", $7, $8, $9}'| \
+#   sort -k1,1 -k2,2n > ES_5tr.bed12.tsv
+
+# ### MXE
+# bedtools groupby -i <(sed '1d' MXE_5tr.pre.bed12.tsv) -g 6 -c 1,2,3,6,4,6,8,9 -o first,min,max,first,first,count,collapse,collapse| \
+#   awk -F'\t' 'BEGIN{OFS="\t";}{print $2, $3, $4, $5, "0", $6, $4, $4, "0", $7, $8, $9}'| \
+#   sort -k1,1 -k2,2n > MXE_5tr.bed12.tsv
+
+# ### ASS
+# bedtools groupby -i <(sed '1d' ASS_5tr.pre.bed12.tsv) -g 6 -c 1,2,3,6,4,6,8,9 -o first,min,max,first,first,count,collapse,collapse| \
+#   awk -F'\t' 'BEGIN{OFS="\t";}{print $2, $3, $4, $5, "0", $6, $4, $4, "0", $7, $8, $9}'| \
+#   sort -k1,1 -k2,2n > ASS_5tr.bed12.tsv
+
+# ### RI
+# bedtools groupby -i <(sed '1d' IR_5tr.pre.bed12.tsv) -g 6 -c 1,2,3,6,4,6,8,9 -o first,min,max,first,first,count,collapse,collapse| \
+#   awk -F'\t' 'BEGIN{OFS="\t";}{print $2, $3, $4, $5, "0", $6, $4, $4, "0", $7, $8, $9}'| \
+#   sort -k1,1 -k2,2n > IR_5tr.bed12.tsv
+
+
+# ### combine ES, MXE, ASS and RI into one BED12 file
+# cat ES_5tr.bed12.tsv MXE_5tr.bed12.tsv ASS_5tr.bed12.tsv IR_5tr.bed12.tsv > AS_5tr.bed12.tsv
+# sort -k4,4 -o AS_5tr_sorted.bed12.tsv AS_5tr.bed12.tsv
+# # # double check all isoforms grouped together
+# # awk -F'\t' -v OFS='\t' '{gsub(/\.retained_intron\..*|\.skip_exon\..*|\.splice_site_at_exon\..*/, "", $4); print}' AS_10tr_sorted.bed12.tsv| cut -f4| uniq| wc -l
+# # 5000
+
+# ### Extract sequences from reference FASTA based on genomic intervals in BED12
+# bedtools getfasta -fi $FASTA -bed AS_5tr_sorted.bed12.tsv -split -nameOnly -fo gencode.vM32.custom.transcriptome.5tr.tmp.fa
+# fold -w 60 gencode.vM32.custom.transcriptome.5tr.tmp.fa > gencode.vM32.custom.transcriptome.5tr.fa
